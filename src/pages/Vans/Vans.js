7c86d93a0d2a5a292949addf675e8915dfc1fd12
Mariusz Rojewski <rojewski@webkon.eu>
2023-06-27 20:11:49 +0200

    07

--- src/pages/Vans/Vans.js
+++ src/pages/Vans/Vans.js
@@ -1,33 +1,58 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 function Vans() {
   const [vans, setVans] = React.useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const typeFilter = searchParams.get("type");
+
+  const displayedVans = typeFilter
+    ? vans.filter((char) => char.type.toLowerCase() === typeFilter)
+    : vans;
+
+  console.log("PARAMS: ", typeFilter);
+
   React.useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
       .then((data) => setVans(data.vans));
   }, []);
 
-  const vanElements = vans.map((van) => (
-    <div key={van.id} className="van-tile">
-      <Link to={`/vans/${van.id}`}>
-        <img src={van.imageUrl} />
-        <div className="van-info">
-          <h3>{van.name}</h3>
-          <p>
-            ${van.price}
-            <span>/day</span>
-          </p>
-        </div>
-        <i className={`van-type ${van.type} selected`}>{van.type}</i>
-      </Link>
-    </div>
-  ));
+  const vanElements = displayedVans.map((van) => {
+    return (
+      <div key={van.id} className="van-tile">
+        <Link to={`/vans/${van.id}`}>
+          <img src={van.imageUrl} />
+          <div className="van-info">
+            <h3>{van.name}</h3>
+            <p>
+              ${van.price}
+              <span>/day</span>
+            </p>
+          </div>
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </Link>
+      </div>
+    );
+  });
 
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
+      <div className="van-list-filter-buttons">
+        <Link to="?type=simple" className="van-type simple">
+          Simple
+        </Link>
+        <Link to="?type=luxury" className="van-type luxury">
+          Luxury
+        </Link>
+        <Link to="?type=rugged" className="van-type rugged">
+          Rugged
+        </Link>
+        <Link to="." className="van-type clear-filters">
+          Clear filter
+        </Link>
+      </div>
       <div className="van-list">{vanElements}</div>
     </div>
   );
