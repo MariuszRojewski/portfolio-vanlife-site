997abf4fd6504771ac8eeb6e5a3ce7dd860fc39c
Mariusz Rojewski <rojewski@webkon.eu>
2023-07-18 17:52:10 +0200

    18.07

--- index.jsx
+++ index.jsx
@@ -23,7 +23,7 @@ import HostVanInfo from './pages/Host/HostVanInfo';
 import HostVanPricing from './pages/Host/HostVanPricing';
 import HostVanPhotos from './pages/Host/HostVanPhotos';
 import NotFound from './pages/NotFound';
-import Login, { loader as loginLoader } from './pages/Login';
+import Login, { loader as loginLoader, action as loginAction } from './pages/Login';
 import Layout from './components/Layout';
 import HostLayout from './components/HostLayout';
 import Error from './components/Error';
@@ -36,7 +36,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
-      <Route path="login" element={<Login />} loader={loginLoader} />
+      <Route path="login" element={<Login />} action={loginAction} />
       <Route
         path="vans"
         element={<Vans />}
--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,65 +1,73 @@
 import React from "react"
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData, useNavigate, redirect, Form } from "react-router-dom"
 import { loginUser } from "../api"
 
 export function loader({ request }) {
     return new URL(request.url).searchParams.get("message")
 }
 
+export async function action({request}) {
+    const formData = await request.formData();
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const data = await loginUser({email, password})
+    console.log(data)
+
+    return null;
+}
+
+// Zadanie rób dalej od tego momentu:
+// https://scrimba.com/learn/reactrouter6/better-but-still-fake-auth-co83844a5a3e46a3ca958268d
+
 export default function Login() {
-    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
-    const [status, setStatus] = React.useState("idle")
-    const [error, setError] = React.useState(null)
-    const message = useLoaderData()
+    // const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    // const [status, setStatus] = React.useState("idle")
+    // const [error, setError] = React.useState(null)
+    // const message = useLoaderData()
+    // const navigate = useNavigate()
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        setStatus("submitting")
-        setError(null)
-        loginUser(loginFormData)
-            .then(data => console.log(data))
-            .catch(err => setError(err))
-            .finally(() => setStatus("idle"))
-    }
-
-    function handleChange(e) {
-        const { name, value } = e.target
-        setLoginFormData(prev => ({
-            ...prev,
-            [name]: value
-        }))
-    }
+    // function handleSubmit(e) {
+    //     e.preventDefault()
+    //     setStatus("submitting")
+    //     setError(null)
+    //     loginUser(loginFormData)
+    //         .then(data => {
+    //             navigate("/host", { replace: true })
+    //         })
+    //         .catch(err => setError(err))
+    //         .finally(() => setStatus("idle"))
+    // }
 
     return (
         <div className="login-container">
             <h1>Sign in to your account</h1>
-            {message && <h3 className="red">{message}</h3>}
-            {error && <h3 className="red">{error.message}</h3>}
+            {/* {message && <h3 className="red">{message}</h3>}
+            {error && <h3 className="red">{error.message}</h3>} */}
 
-            <form onSubmit={handleSubmit} className="login-form">
+            <Form 
+                action="/login"
+                method="post"
+                className="login-form">
                 <input
                     name="email"
-                    onChange={handleChange}
                     type="email"
                     placeholder="Email address"
-                    value={loginFormData.email}
                 />
                 <input
                     name="password"
-                    onChange={handleChange}
                     type="password"
                     placeholder="Password"
-                    value={loginFormData.password}
                 />
                 <button 
-                    disabled={status === "submitting"}
+                    // disabled={status === "submitting"}
                 >
-                    {status === "submitting" 
+                    Wyślij
+                    {/* {status === "submitting" 
                         ? "Logging in..." 
                         : "Log in"
-                    }
+                    } */}
                 </button>
-            </form>
+            </Form>
         </div>
     )
 }
